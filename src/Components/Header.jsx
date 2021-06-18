06b370ced4e185d3da8ebaa2dd39b6574b2b2ead
ralph <wealthralph@gmail.com>
2021-06-18 01:10:03 +0100

    Reverting to c5520b8

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
 import {FaBars, FaTimes } from 'react-icons/fa'
-import Button from './Button'
 
 const Header = () => {
 
@@ -19,34 +18,6 @@ const Header = () => {
                 <div className="menu-icon" onClick={handleClick}>
                     {click ? <FaTimes/> : <FaBars/>}
                 </div>
-
-                <ul className= {click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className="nav-item">
-                        <Link to='/' className= 'nav-links'>
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/' className= 'nav-links'>
-                            Projects
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/' className= 'nav-links'>
-                            Thoughts
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        { button ? (<Link  className= 'btn-links'>
-                            <Button buttonStyle= 'btn--outline'>Contact me</Button>
-                        </Link>) : (
-                            <Link className="btn-link">
-                                <Button buttonStyle='btn--outline' buttonSize='btn--mobile'></Button>
-                            </Link>
-                        ) }
-                    </li>
-
-                </ul>
                 
             </div>
         </div>
